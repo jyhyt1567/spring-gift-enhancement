e21d18f458dfd36eb4b3732cd1c4e4f573764232
jyhyt1567 <jyhyt1567@gmail.com>
2025-07-15 16:02:42 +0900

    feat: 사용하지 않는 sql 파일 제거

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into products(name, price, imageUrl) values('아이스아메리카노', 2500, 'abcdef');
-
-insert into products(name, price, imageUrl) values('붕어빵', 1000, 'abcdef');
-
-insert into products(name, price, imageUrl) values('식빵', 5000, 'abcdef');
