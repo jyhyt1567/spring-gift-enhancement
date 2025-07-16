88f382322eb17b32baf139213e88e704d8dad3db
jyhyt1567 <jyhyt1567@gmail.com>
2025-07-16 15:35:55 +0900

    test: 테스트 코드 수정

--- src/test/java/gift/repository/WishRepositoryTest.java
+++ src/test/java/gift/repository/WishRepositoryTest.java
@@ -27,8 +27,8 @@ public class WishRepositoryTest {
 
     @Test
     void save() {
-        Product product = new Product("아이스아메리카노", 1500L, "asd.dsa");
-        Member member = new Member("asd@asd.asd", "dasdada", "user");
+        Product product = new Product(1L, "아이스아메리카노", 1500L, "asd.dsa");
+        Member member = new Member(1L, "asd@asd.asd", "dasdada", "user");
         Wish expected = new Wish(product, member, 4L);
         wishRepository.save(expected);
         Long id = expected.getId();
