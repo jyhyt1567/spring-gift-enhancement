d7823098bf91cc4cd4f14cd2fa4fecda8dadd605
jyhyt1567 <jyhyt1567@gmail.com>
2025-07-17 13:56:42 +0900

    feat: 초기 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,7 +14,20 @@ insert into products(name, price, image_url) values('식빵4', 5004, 'abcdef');
 
 insert into products(name, price, image_url) values('식빵5', 5005, 'abcdef');
 
-
 insert into members(email, password, role) values('testUser1@asdasd.asd', 'asd', 'user');
 
 insert into wishes(product_id, member_id, quantity) values(1,1,5);
+
+insert into wishes(product_id, member_id, quantity) values(2,1,6);
+
+insert into wishes(product_id, member_id, quantity) values(3,1,2);
+
+insert into wishes(product_id, member_id, quantity) values(4,1,6);
+
+insert into wishes(product_id, member_id, quantity) values(5,1,1);
+
+insert into wishes(product_id, member_id, quantity) values(6,1,6);
+
+insert into wishes(product_id, member_id, quantity) values(7,1,8);
+
+insert into wishes(product_id, member_id, quantity) values(8,1,10);
